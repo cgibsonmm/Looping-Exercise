ebe4306ade72a23dc5ae4f96c631f5069f31c0f0
Steven VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-07-01 13:24:21 -0600

    full solution

--- exercises.js
+++ exercises.js
@@ -57,5 +57,13 @@ for (let i = 0; i < foodsArray.length; i++) {
 
 //Exercise 4
 
+let accountBalance = 0
+
+while (accountBalance <= 100) {
+  // let accountBalance = 0
+  let message = `My checking account balance is $${accountBalance}`
+  accountBalance++
+  console.log(message)
+}
 
 //Bonus
