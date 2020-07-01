ea8975e2c673d86194c6aa9b0a29f07fa26ec2d9
Steven VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-07-01 12:46:21 -0600

    number 1 complete

--- exercises.js
+++ exercises.js
@@ -1,5 +1,21 @@
 //Exercise 1
 
+const temp = 100
+
+if (temp <= 32) {
+  console.log("Watch out for ice")
+} else if (temp >= 33 && temp <= 55) {
+  console.log("Better put on a jacket.")
+} else if (temp >= 56 && temp <= 70) {
+  console.log( "Such nice weather we're having")
+} else if (temp >= 71 && temp <= 99) {
+  console.log( "Such nice weather we're having")
+} else if (temp === 212 || temp === 444) {
+  console.log("Where are you exactly?")
+} else {
+  console.log("Global Warming!!!")
+}
+
 //Exercise 2
 
 //Exercise 3
