f97a21dfbe798e7da897021311af066908f34b84
Steven VanWoerkom <steven.vanwoerkom@generalassemb.ly>
2020-07-01 13:14:22 -0600

    Excersice 2 & 3

--- exercises.js
+++ exercises.js
@@ -1,24 +1,59 @@
 //Exercise 1
 
-const temp = 100
+// const temp = 100
 
-if (temp <= 32) {
-  console.log("Watch out for ice")
-} else if (temp >= 33 && temp <= 55) {
-  console.log("Better put on a jacket.")
-} else if (temp >= 56 && temp <= 70) {
-  console.log( "Such nice weather we're having")
-} else if (temp >= 71 && temp <= 99) {
-  console.log( "Such nice weather we're having")
-} else if (temp === 212 || temp === 444) {
-  console.log("Where are you exactly?")
-} else {
-  console.log("Global Warming!!!")
-}
+// if (temp <= 32) {
+//   console.log("Watch out for ice")
+// } else if (temp >= 33 && temp <= 55) {
+//   console.log("Better put on a jacket.")
+// } else if (temp >= 56 && temp <= 70) {
+//   console.log( "Such nice weather we're having")
+// } else if (temp >= 71 && temp <= 99) {
+//   console.log( "Such nice weather we're having")
+// } else if (temp === 212 || temp === 444) {
+//   console.log("Where are you exactly?")
+// } else {
+//   console.log("Global Warming!!!")
+// }
+
 
 //Exercise 2
 
+// let month = new Date().getMonth()
+// console.log(month)
+// // month = month.getMonth()
+// console.log(month)
+// month++
+// // month += 1
+// console.log(month)
+
+// let isJuly = (month === 7) ? 'It is July!' : 'It isn\'t July'
+// console.log(isJuly)
+
 //Exercise 3
+
+let foodsArray = ['almonds', 'potato chips', 'ice cream', 'filet mignon', 'tacos'];
+
+for (let i = 0; i < foodsArray.length; i++) {
+  // console.log(foodsArray[i])
+  switch (foodsArray[i]) {
+    case 'almonds':
+      console.log(`${foodsArray[i]} a healthy snack`)
+      break;
+    case 'potato chips':
+      console.log( 'Something salty, nice!')
+      break;
+    case 'ice cream':
+      console.log('Something sweet, ok!')
+      break;
+    case 'filet mignon':
+      console.log('Eating like royalty tonight')
+      break;
+    default:
+      console.log('I don\'t know what to eat.')
+  }
+}
+
 
 //Exercise 4
 
